43a6fdb19aedbf441a99c48bbfa2e87ee4b73c41
0mni-gears515 <adamyi515@gmail.com>
2022-10-12 13:25:18 -0500

    Add functionality to FeedbackStats component. It now show average rating and number of reviews

--- src/components/feedback-stats/feedback-stats.component.jsx
+++ src/components/feedback-stats/feedback-stats.component.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './feedback-stats.style.css';
 
+// Context
+import FeedbackContext from '../../context/feedback.context';
+
 const FeedbackStats = () => {
+    const { feedbacks } = useContext(FeedbackContext);
+
+    // Private methods //////////////////////////////////////////////////////
+    const averageRating = feedbacks.reduce((prev, current) => {
+        return prev += current.rating
+    }, 0) / feedbacks.length;
+
+    console.log(averageRating);
+
     return(
         <div className='feedback-stats'>
-            <p className='feedback-stats__review'>10 Reviews</p>
-            <p className='feedback-stats__rating'>Average rating: 4.33</p>
+            <p className='feedback-stats__review'>{feedbacks.length} Reviews</p>
+            <p className='feedback-stats__rating'>Average rating: {averageRating.toFixed(2)}</p>
         </div>
     )
 }
